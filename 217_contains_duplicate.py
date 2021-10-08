8096c03103d35c3f1bdd40d57cc39a0af4e969d5
Amir Raza <amir9ume@gmail.com>
2021-10-08 11:19:30 -0400

    solution problem 217

---
+++ 217_contains_duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        map={}
+        
+        for num in nums:
+            if num not in map:
+                map[num]=True
+            else:
+                return True
+        
